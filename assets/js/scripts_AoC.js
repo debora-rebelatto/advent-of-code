05a0582d3ea24e251c3be0bdb7bcad62de9f862c
debora-rebelatto <deborarebelatto1@gmail.com>
2022-11-22 22:44:46 -0300

    feat: updated html, reduced css by a LOT and corrected the copy input

--- assets/js/scripts_AoC.js
+++ assets/js/scripts_AoC.js
@@ -1,9 +1,10 @@
-const copyText = (value) => {
-  var copyText = document.getElementById("copyvalue");
+const copyText = () => {
+  var copyText = document.getElementById("code-leaderboard");
   copyText.select();
   copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
 }
 
 const handleClick = () => {
-  document.querySelector(".nav-menu").classList.toggle("active");
+  document.querySelector(".mobile-menu").classList.toggle("active");
 };
